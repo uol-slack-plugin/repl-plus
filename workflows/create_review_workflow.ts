652093439fb636e29c07d6104637f55560ec23bd
egmp7 <deweyerick@gmail.com>
2024-01-12 20:02:48 -0600

    Open Form Init

--- workflows/create_review_workflow.ts
+++ workflows/create_review_workflow.ts
@@ -8,9 +8,40 @@ const CreateReviewWorkflow = DefineWorkflow({
       interactivity: {
         type: Schema.slack.types.interactivity,
       },
+      channel: {
+        type: Schema.slack.types.channel_id,
+      },
     },
     required: ["interactivity"],
   },
+});
+
+const inputForm = CreateReviewWorkflow.addStep(
+  Schema.slack.functions.OpenForm,
+  {
+    title: "Send a greeting",
+    interactivity: CreateReviewWorkflow.inputs.interactivity,
+    submit_label: "Send greeting",
+    fields: {
+      elements: [{
+        name: "channel",
+        title: "Channel to send message to",
+        type: Schema.slack.types.channel_id,
+        default: CreateReviewWorkflow.inputs.channel,
+      }, {
+        name: "message",
+        title: "Message to recipient",
+        type: Schema.types.string,
+        long: true,
+      }],
+      required: ["recipient", "channel", "message"],
+    },
+  },
+);
+
+CreateReviewWorkflow.addStep(Schema.slack.functions.SendMessage, {
+  channel_id: inputForm.outputs.fields.channel,
+  message: inputForm.outputs.fields.message,
 });
 
 export default CreateReviewWorkflow;
