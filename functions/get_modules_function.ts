d7226663daf6089f6d73d01c4c74b91451b37d92
egmp7 <deweyerick@gmail.com>
2024-01-17 17:42:49 -0600

    two test passed with API mock calls

--- functions/get_modules_function.ts
+++ functions/get_modules_function.ts
@@ -1,4 +1,4 @@
-import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
+import { DefineFunction, SlackFunction } from "deno-slack-sdk/mod.ts";
 import { ModuleType } from "../types/module_type.ts";
 import { MODULES_DATASTORE_NAME } from "../datastores/modules_datastore.ts";
 
@@ -13,12 +13,10 @@ export const GetModulesDefinition = DefineFunction({
   output_parameters:{
     properties:{ 
       modules : {type: ModuleType},
-      payload: {type: Schema.types.object}
      },
       
     required:[]
   }
-
 });
 
 // IMPLEMENTATION
@@ -30,15 +28,16 @@ export default SlackFunction(
     console.log("Get Modules Inputs: ", inputs)
 
     // call the API
-    const res = await await client.apps.datastore.query({
+    const res = await client.apps.datastore.query({
       datastore: MODULES_DATASTORE_NAME,
     }); 
 
-    console.log(res.ok);
-    
-    const modules = [{id:"111",rare:"no belong"}]
-    const payload = {ok:res.ok}
+    // handle datastore error
+    if (!res.ok) {
+      const queryErrorMsg = `Error accessing modules datastore (Error detail: ${res.error})`;
+      return { error: queryErrorMsg};
+    }
 
-    return { outputs: { modules, payload } };
+    return {outputs:{}}
   },
 );
--- functions/get_modules_function_test.ts
+++ functions/get_modules_function_test.ts
@@ -1,17 +1,39 @@
 import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
 import { GET_MODULES_CALLBACK_ID } from "./get_modules_function.ts";
 import GetModules from "./get_modules_function.ts";
-import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
-
+import { assertExists } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
 
 const { createContext } = SlackFunctionTester(GET_MODULES_CALLBACK_ID);
 
-Deno.test("Retrieve an object", async ()=>{
-  const {outputs} = await GetModules(createContext({ inputs:{}}));
-  assertEquals(typeof outputs?.modules, "object" )
-})
+const inputs = {};
 
-Deno.test("Call the API and retrieve payload with a property ok set to true", async ()=>{
-  const {outputs} = await GetModules(createContext({ inputs:{}}));
-  assertEquals(outputs?.payload, {ok: false} )
-})
+mf.install();
+
+Deno.test("returns an outputs object if successfully calls the API ", async () => {
+  mf.mock("POST@/api/apps.datastore.query", () => {
+    return new Response(
+      `{"ok": true}`,
+      {
+        status: 200,
+      },
+    );
+  });
+
+  const { outputs } = await GetModules(createContext({ inputs }));
+  assertExists(outputs);
+});
+
+Deno.test("returns an error if datastore fails (apps.datastore.query) ", async () => {
+  mf.mock("POST@/api/apps.datastore.query", () => {
+    return new Response(
+      `{"ok": false,"error":"hello error"}`,
+      {
+        status: 200,
+      },
+    );
+  });
+
+  const { error } = await GetModules(createContext({ inputs }));
+  assertExists(error);
+});
