197761fd521beb5e587c6706504256c817a10e72
egmp7 <deweyerick@gmail.com>
2024-01-17 15:52:27 -0600

    unit testing: two test passed

--- functions/get_modules_function.ts
+++ functions/get_modules_function.ts
@@ -1,20 +1,44 @@
-import { DefineFunction, SlackFunction } from "deno-slack-sdk/mod.ts";
+import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
+import { ModuleType } from "../types/module_type.ts";
+import { MODULES_DATASTORE_NAME } from "../datastores/modules_datastore.ts";
 
+// CONSTANTS
 export const GET_MODULES_CALLBACK_ID = "get_modules_function"
 
+// DEFINITION
 export const GetModulesDefinition = DefineFunction({
   callback_id: GET_MODULES_CALLBACK_ID,
   title: "Get modules function",
   source_file: "functions/get_modules_function.ts",
+  output_parameters:{
+    properties:{ 
+      modules : {type: ModuleType},
+      payload: {type: Schema.types.object}
+     },
+      
+    required:[]
+  }
 
 });
 
+// IMPLEMENTATION
 export default SlackFunction(
   GetModulesDefinition,
-  ({ inputs, }) => {
+  async ({ inputs, client }) => {
 
+    // Debug
     console.log("Get Modules Inputs: ", inputs)
 
-    return { outputs: { } };
+    // call the API
+    const res = await await client.apps.datastore.query({
+      datastore: MODULES_DATASTORE_NAME,
+    }); 
+
+    console.log(res.ok);
+    
+    const modules = [{id:"111",rare:"no belong"}]
+    const payload = {ok:res.ok}
+
+    return { outputs: { modules, payload } };
   },
 );
--- functions/get_modules_function_test.ts
+++ functions/get_modules_function_test.ts
@@ -1,11 +1,17 @@
 import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
 import { GET_MODULES_CALLBACK_ID } from "./get_modules_function.ts";
 import GetModules from "./get_modules_function.ts";
+import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+
 
 const { createContext } = SlackFunctionTester(GET_MODULES_CALLBACK_ID);
 
-Deno.test("Output should be an empty object", async ()=>{
-  await GetModules(createContext({ inputs:{}}));
-  
+Deno.test("Retrieve an object", async ()=>{
+  const {outputs} = await GetModules(createContext({ inputs:{}}));
+  assertEquals(typeof outputs?.modules, "object" )
 })
 
+Deno.test("Call the API and retrieve payload with a property ok set to true", async ()=>{
+  const {outputs} = await GetModules(createContext({ inputs:{}}));
+  assertEquals(outputs?.payload, {ok: false} )
+})
