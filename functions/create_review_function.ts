2f0cdf9df47ebe323663e4858a960c960cbccee1
egmp7 <deweyerick@gmail.com>
2024-01-18 14:23:45 -0600

    test1 and test2 passed

--- functions/create_review_function.ts
+++ functions/create_review_function.ts
@@ -1,5 +1,6 @@
 import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
 import { ModulesArrayType } from "../types/modules.ts";
+import { REVIEWS_DATASTORE_NAME } from "../datastores/reviews_datastore.ts";
 
 export const CREATE_REVIEW_FUNCTION_CALLBACK_ID = "create_review_function";
 
@@ -27,7 +28,32 @@ export const CreateReviewFunction = DefineFunction({
   },
 });
 
-export default SlackFunction(CreateReviewFunction, ({ inputs }) => {
+export default SlackFunction(CreateReviewFunction, async({ inputs, client }) => {
   console.log("Create review function inputs: ",inputs);
-  return { outputs: {} };
+
+  const reviewID = crypto.randomUUID();
+
+  const res = await client.apps.datastore.put({
+    datastore: REVIEWS_DATASTORE_NAME,
+    item: {
+      id: reviewID,
+      module_id: "111",
+      user_id: inputs.user_id,
+      review: inputs.review,
+      time_consumption: inputs.time_consumption,
+      rating_quality: inputs.rating_quality,
+      rating_difficulty: inputs.rating_difficulty,
+      rating_learning: inputs.rating_learning,
+      created_at: Date.now(),
+      updated_at: Date.now()
+    }})
+
+    if (!res.ok) {
+      const queryErrorMsg = `Error when creating a new entry for reviews datastore (Error detail: ${res.error})`;
+      return { error: queryErrorMsg};
+    }
+
+    console.log("RES",res)
+
+  return { outputs:{} };
 });
--- functions/create_review_function_test.ts
+++ functions/create_review_function_test.ts
@@ -1,18 +1,48 @@
 import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
-import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
-import { CREATE_REVIEW_FUNCTION_CALLBACK_ID} from "./create_review_function.ts";
+import { assertExists } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+import { CREATE_REVIEW_FUNCTION_CALLBACK_ID } from "./create_review_function.ts";
 import CreateReviewFunction from "./create_review_function.ts";
+import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
 
-const { createContext } = SlackFunctionTester(CREATE_REVIEW_FUNCTION_CALLBACK_ID);
+mf.install();
 
-Deno.test("Output should be an empty object", async ()=>{
-  const inputs = {
-      user_id : "223123",
-      module_name: "hello",
-      review: "review"
-  };
+const inputs = {
+  user_id: "223123",
+  module_name: "name test",
+  review: "review",
+  modules: [{ id: "1a", code: "CM0000", name: "name test", rating: 1.2 }],
+};
+
+const { createContext } = SlackFunctionTester(
+  CREATE_REVIEW_FUNCTION_CALLBACK_ID,
+);
+
+// TEST 1
+Deno.test("returns an outputs object if successfully calls the API", async () => {
+  mf.mock("POST@/api/apps.datastore.put", () => {
+    return new Response(
+      `{"ok": true}`,
+      {
+        status: 200,
+      },
+    );
+  });
 
   const { outputs } = await CreateReviewFunction(createContext({ inputs }));
-  
-  assertEquals(outputs,{})
-})
+  assertExists(outputs);
+});
+
+// TEST 2
+Deno.test("returns an error if the API call fails (apps.datastore.query", async () => {
+  mf.mock("POST@/api/apps.datastore.put", () => {
+    return new Response(
+      `{"ok": false}`,
+      {
+        status: 200,
+      },
+    );
+  });
+
+  const { error } = await CreateReviewFunction(createContext({ inputs }));
+  assertExists(error);
+});
