73e7757a552f113aad1c71b73344f3e069babdc8
egmp7 <deweyerick@gmail.com>
2024-01-27 11:48:37 -0600

    create review back and review_entry type

---
+++ types/review_entry.ts
@@ -0,0 +1,9 @@
+export type ReviewEntry = {
+  module: string | null;
+  quality : string | null;
+  difficulty: string | null;
+  time: string | null;
+  learning: string | null;
+  title: string | null;
+  content: string | null;
+}
