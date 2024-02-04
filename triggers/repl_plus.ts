2c6fbc012e822896566b389af37804aeab2c4391
egmp7 <deweyerick@gmail.com>
2024-02-04 13:31:45 -0600

    hide google form

--- triggers/repl_plus.ts
+++ triggers/repl_plus.ts
@@ -1,11 +1,11 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
 import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
-import DashboardWorkflow from "../workflows/repl_plus_workflow.ts";
+import ReplPlusWorkflow from "../workflows/repl_plus_workflow.ts";
 
-const ReplPlusTrigger: Trigger<typeof DashboardWorkflow.definition> = {
+const ReplPlusTrigger: Trigger<typeof ReplPlusWorkflow.definition> = {
   type: TriggerTypes.Shortcut,
   name: "REPL Plus Trigger",
-  workflow: `#/workflows/${DashboardWorkflow.definition.callback_id}`,
+  workflow: `#/workflows/${ReplPlusWorkflow.definition.callback_id}`,
   inputs: {
     user_id: {
       value: TriggerContextData.Shortcut.user_id,
